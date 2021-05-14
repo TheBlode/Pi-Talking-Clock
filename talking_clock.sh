e4c529fdd18cd9617233050a79390e955c89c1e2
TheBlode <theblode2020@gmail.com>
2021-05-14 20:41:00 +0100

    - initial commit.
    - initial commit.

---
+++ talking_clock.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# ============================
+# Pi Talking Clock
+# Written by The Blode
+# 10/05/21
+# This script will run a talking clock on your Raspberry Pi
+# ============================
+
+# Get time
+hour=$(date +%H)
+minute=$(date +%M)
+
+# Get AM or PM
+if [ $hour > 11 ]
+    then
+        # Set PM
+        ampm="PM"
+
+        # Get 12 hour clock hour
+        if [ $hour != 12 ]
+            then
+                hour=$(($hour-12))
+        fi
+else
+    # Set AM
+    ampm="AM"
+fi
+
+# Generate sound file
+sudo pico2wave -w /home/pi/Desktop/time.wav "$hour, $minute, $ampm" > /dev/null 2>&1
+
+# Play sound
+sudo mplayer -af volume=4.8 /home/pi/Desktop/time.wav > /dev/null 2>&1
+
+# Sleep for a few seconds
+sleep 5s
+
+# Repeat again
+sudo mplayer -af volume=4.8 /home/pi/Desktop/time.wav > /dev/null 2>&1
+
+# Sleep for a few seconds
+sleep 5s
+
+# Repeat again
+sudo mplayer -af volume=4.8 /home/pi/Desktop/time.wav > /dev/null 2>&1
+
+# Exit
+exit
